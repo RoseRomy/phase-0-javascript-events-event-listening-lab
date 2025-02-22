493e4bf453aaeee99b0ecb275e767768c4e18382
+ <RoseKamau@Romys-Macboook-Air.local>
2025-02-22 23:09:03 +0300

    Addes index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const button = document.getElementById("button");
+    if (!button) {
+        console.error("Button not found!");
+        return;
+    }
+    button.addEventListener("click", function() { 
+        console.log("Button clicked!")     
+    });
 }
